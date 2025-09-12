f5fe9f2e89df4e4b805c307127f5e90f736b331e
Arewa Tolulope <tolulopearewa@student.oauife.edu.ng>
2025-09-12 12:41:38 +0100

    feat: Remove commented-out GenreSection import and usage from MovieShows component

--- src/pages/Movie-ShowsPage.tsx
+++ src/pages/Movie-ShowsPage.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header"
 import Watch from "../components/Watch"
-// import GenreSection from "../components/GenreSection"
 import Movies from "../components/MoviesCategory"
 import TrialBG from "../components/TrialBG"
 import Footer from "../components/Footer"
@@ -10,7 +9,6 @@ function MovieShows() {
       <Header />
       <Watch />
       <div className="space-y-10 mt-10">
-        {/* <GenreSection /> */}
         <Movies />
         <TrialBG />
         <Footer />
